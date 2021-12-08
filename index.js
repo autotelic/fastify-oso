30add4b0d82d70a68573b6b93096aaa3b65f3c55
jkirkpatrick24 <jkirkpat16@gmail.com>
2021-12-08 11:39:22 -0800

    test

--- index.js
+++ index.js
@@ -3,18 +3,20 @@
 const fp = require('fastify-plugin')
 const { Oso } = require('oso')
 
-const defaultOptions = {
-  setupOso: async oso => oso
-}
+const AUTHORIZE_REQUEST = 'authorizeRequest'
 
 async function fastifyOso (fastify, options) {
-  const { setupOso } = {
-    ...options,
-    ...defaultOptions
-  }
+  const { setupOso } = options
   const oso = new Oso()
+
   await setupOso(oso)
+
+  function createOsoDecorator (key) {
+    return [key, async (...args) => oso[key](...args)]
+  }
+
   fastify.decorate('oso', oso)
+  fastify.decorateRequest(...createOsoDecorator(AUTHORIZE_REQUEST))
 }
 
 module.exports = fp(fastifyOso, { name: 'fastify-oso' })
