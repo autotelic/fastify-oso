bd90f42e3a57e1428191274d44e3d3451c19a3b4
jkirkpatrick24 <jkirkpat16@gmail.com>
2021-12-08 16:56:59 -0800

    docs: Add onRequestHook example

---
+++ examples/onRequestHook/index.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const { fastifyOso } = require('../..')
+
+const PORT = process.env.PORT || 3000
+
+const app = require('fastify')()
+
+const opts = {
+  async setupOso (oso) {
+    // Load the Authorization rules.
+    await oso.loadFiles(['authorization.polar'])
+    return oso
+  }
+}
+
+// Register fastify-oso
+app.register(fastifyOso, opts)
+
+// For every request, this hook will run. If a request is unauthorized, authorizeRequest will throw
+// an oso authorization error. We catch that and respond to the client with a `403` code.
+app.addHook('onRequest', async function (request, reply) {
+  try {
+    await request.authorizeRequest({}, request)
+  } catch (error) {
+    reply.status(403).send('Access Denied')
+  }
+})
+
+// Requests here are allowed.
+app.get('/public', (request, response) => 'public information')
+
+// Requests here will not be allowed.
+app.get('/private', (request, reply) => 'super secret')
+
+app.listen(PORT, (_, address) => {
+  console.log(`listening at ${address}`)
+})
--- index.js
+++ index.js
@@ -11,12 +11,12 @@ async function fastifyOso (fastify, options) {
 
   await setupOso(oso)
 
-  function createOsoDecorator (key) {
-    return [key, async (...args) => oso[key](...args)]
+  function wrapDecorator (key) {
+    return async (...args) => oso[key](...args)
   }
 
   fastify.decorate('oso', oso)
-  fastify.decorateRequest(...createOsoDecorator(AUTHORIZE_REQUEST))
+  fastify.decorateRequest(AUTHORIZE_REQUEST, wrapDecorator(AUTHORIZE_REQUEST))
 }
 
 module.exports = fp(fastifyOso, { name: 'fastify-oso' })
